f460cb7acac6903aceaf3e766e70b311bab579f5
Natasha Hall <nhall@vision-ins.net>
2019-03-23 00:18:07 -0700

    Checkpoint 4 List and Search Completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,6 +10,8 @@ module.exports = class MenuController {
 				message: "Please choose from an option below: ",
 				choices: [
 					"Add new contact",
+					"View all contacts",
+					"Searc for contact",
 					"Exit"
 				]
 			}
@@ -25,6 +27,12 @@ module.exports = class MenuController {
 				case "Add new contact":
 					this.addContact();
 				 	break;
+				case "View all contacts":
+					this.getContacts();
+					break;
+				case "Search for a contact":
+					this.search();
+					break;
 				case "Exit":
 					this.exit();
 				default:
@@ -45,7 +53,7 @@ module.exports = class MenuController {
 	addContact(){
 		this.clear();
 		inquirer.prompt(this.book.addContactQuestions).then((answers) => {
-			this.book.addContact(answers.name, answers.phone).then((contact) => {
+			this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
 				console.log("Contact added successfully!");
 				this.main();
 			}).catch((err) => {
@@ -56,13 +64,105 @@ module.exports = class MenuController {
 		
 	}
 
+	getContacts(){
+		this.clear();
+
+		this.book.getContacts().then((contacts) => {
+			for (let contact of contacts) {
+				console.log(`
+				name: ${contact.name}
+				phone number: ${contact.phone}
+				email: ${contact.email}
+				---------------`
+				);
+			}
+			this.main();
+		}).catch((err) => {
+			console.log(err);
+			this.main();
+		});
+
+	}
+
+	getContactCount() {
+		return this.contacts.length;
+	}
+
+	search(){
+		inquirer.prompt(this.book.searchQuestions)
+		.then((target) => {
+			this.book.search(target.name)
+			.then((contact) => {
+				if(contact === null){
+					this.clear();
+					console.log("contact not found");
+					this.search();
+				} else {
+					this.showContact(contact);
+				}
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			this.main();
+		});
+	}
+
+	showContact(contact){
+		this._printContact(contact);
+		inquirer.prompt(this.book.showContactQuestions)
+		.then((answer) => {
+			switch(answer.selected){
+				case "Delete contact":
+					this.delete(contact);
+					break;
+				case "Main menu":
+					this.main();
+					break;
+				default:
+					console.log("Something went wrong.");
+					this.showContact(contact);		
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			this.showContact(contact);
+		});
+	}
+
+	_printContact(contact){
+		console.log(`
+			name. ${contact.name}
+			phone number: ${contact.phone}
+			email: ${contact.email}
+			----------------`
+		);
+	}
+
+	delete(contact){
+		inquirer.prompt(this.book.deleteConfirmQuestions)
+		.then((answre) => {
+			if(answer.confirmation){
+				this.book.delete(contact.id);
+				console.log("Contact deleted!";
+				this.main();
+			} else {
+				console.log("Contact not deleted");
+				this.showContact(contact);
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			this.main();
+		});
+	}
+
 	exit(){
 		console.log("Thanks for using AddressBloc!");
 		process.exit();
 	}
 
 
-	getContactCount() {
-		return this.contacts.length;
-	}
+	
+
 }
